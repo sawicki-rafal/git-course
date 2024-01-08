ce8a6bccaa540f46330f6f9fd559aa78acb4f061
Rafal <sawrafdev@outlook.com>
2024-01-08 18:20:07 +0100

    Init repo

---
+++ src/main/java/pl/edukacyjni/gitcourse/GitCourseApplication.java
@@ -0,0 +1,13 @@
+package pl.edukacyjni.gitcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edukacyjni/gitcourse/GitCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edukacyjni.gitcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
