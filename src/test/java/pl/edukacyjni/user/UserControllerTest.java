f5467546f96b8c049cd4fc3376691a67ade8ee84
Rafal <sawrafdev@outlook.com>
2024-01-09 04:21:27 +0100

    Add tests

---
+++ src/test/java/pl/edukacyjni/user/UserControllerTest.java
@@ -0,0 +1,45 @@
+package pl.edukacyjni.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+    private static final String BASE_ENDPOINT = "/api/users";
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetAllUsers() throws Exception {
+        // arrange
+        List<User> userList = Arrays.asList(
+                new User(1L, "user1", "example"),
+                new User(2L, "user2", "use2om")
+        );
+        when(userService.getAllUsers()).thenReturn(userList);
+
+        // act + assert
+        mockMvc.perform(get(BASE_ENDPOINT))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].username").value("user1"))
+                .andExpect(jsonPath("$[1].username").value("user2"));
+    }
+}
