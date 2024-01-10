a19594f76a28c460e92ada65192e488764295b5a
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2024-01-10 16:50:02 +0100

    added tests for email validation

---
+++ src/test/java/pl/edukacyjni/user/UserServiceTests.java
@@ -0,0 +1,40 @@
+package pl.edukacyjni.user;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.edukacyjni.pair.Pair;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTests {
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void checkEmailValidity_ShouldReturnTrue_WhenValidEmailAddressGiven() {
+        assertTrue(userService.checkEmailValidity(new Pair(
+                "valid.email@gmail.com"
+                , "alsovalidEmAil@o2.pl")));
+    }
+    @Test
+    public void checkEmailValidity_ShouldReturnFalse_WhenInvalidEmailAddressGiven() {
+        assertAll(
+                () -> assertFalse(userService.checkEmailValidity(new Pair(
+                        ".email@gmail.com"
+                        , "@o2.pl"))),
+                () -> assertFalse(userService.checkEmailValidity(new Pair(
+                        "A.V@.com"
+                        , "val@o2"))),
+                () -> assertFalse(userService.checkEmailValidity(new Pair(
+                        ""
+                        , "a.a@o.2"))),
+                () -> assertFalse(userService.checkEmailValidity(new Pair(
+                        ".email@"
+                        , "e@o2.pl$")))
+        );
+    }
+
+}
