787befcde5e2fe8d65d646e060437781090ab0e9
marekkaczbartek <bartek.kaczmarek03@gmail.com>
2024-01-13 01:28:37 +0100

    Add endpoints for getting users by id and deactivating users

--- src/main/java/pl/edukacyjni/user/UserService.java
+++ src/main/java/pl/edukacyjni/user/UserService.java
@@ -5,19 +5,41 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     private static final List<User> USERS = new ArrayList<>(){{
-        add(new User(1L, "michaelPaello", "kochamPierogi"));
-        add(new User(2L, "theGhostCasper", "huliganJezusaSt0"));
-        add(new User(3L, "stNicolas", "haslo123!"));
-        add(new User(4L, "santiago", "deadline"));
-        add(new User(5L, "was-far", "abcd"));
+        add(new User(1L, "michaelPaello", "kochamPierogi", false));
+        add(new User(2L, "theGhostCasper", "huliganJezusaSt0", true));
+        add(new User(3L, "stNicolas", "haslo123!", false));
+        add(new User(4L, "santiago", "deadline", true));
+        add(new User(5L, "was-far", "abcd", true));
     }};
 
     public List<User> getAllUsers() {
         return USERS;
+    }
+
+    public Optional<User> getUserById(Long id) {
+        for (User user : USERS) {
+            if (id.equals(user.getId())) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<User> deactivateUserById(Long id) {
+        Optional<User> userOptional = getUserById(id);
+        if (userOptional.isPresent() && userOptional.get().getIsActive()) {
+            User user = userOptional.get();
+            user.setIsActive(false);
+            return Optional.of(user);
+        }
+        else {
+            return userOptional;
+        }
     }
 }
